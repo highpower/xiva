9c207696867cadb29a4ccc1e0e66975baa285a9b
Leonid Khachaturov <leonya@leonid-khachaturovs-macbook-pro.local>
2010-09-08 00:37:42 +0200

    Basic posting/receiving of messages implemented

--- examples/xiva-web-chat/index.js
+++ examples/xiva-web-chat/index.js
@@ -1,11 +1,43 @@
 $(document).ready(function() {
+  var inited = false;
+  
   var bMessages = $('.b-messages');
   var messageArea = $('.b-writemessage__area');
-  var messageSend = $('.b-writemessage__send');
 
   var userLinks = $('a.b-userlist__user');
   var roomLinks = $('.b-rooms__room a');
+  
+  // WebSocket implementation
+  WebSocket.__swfLocation = 'web-socket-js/WebSocketMain.swf';
 
+  var ws;
+  
+  function init() {
+    // Connect to Web Socket.
+    // Change host/port here to your own Web Socket server.
+    ws = new WebSocket('ws://leonya.dev.yandex.net:8881/?id=chat&channels=rooms_all,room_1,room_2,room_3');
+
+    // Set event handlers.
+    ws.onopen = function() {
+      if (inited === false) {
+        messageArea.get(0).disabled = false;
+        messageArea.val('');
+        inited = true;
+      }
+    };
+    
+    ws.onmessage = function(e) {
+      receiveMessage(e.data);
+    };
+    
+    ws.onclose = function() {
+      setTimeout(init, 1000);
+    };
+  }
+  
+  init();
+	
+  // UI
   messageArea.keydown(function(e) {
     if (e.keyCode == 13) {
       e.preventDefault();
@@ -17,11 +49,6 @@ $(document).ready(function() {
     }
   });
 
-  messageSend.click(function(e) {
-    e.preventDefault();
-    postMessage();
-  });
-
   userLinks.click(function(e) {
     e.preventDefault();
     messageArea.val('@' + $(e.target).html() + ' ');
@@ -31,9 +58,21 @@ $(document).ready(function() {
   roomLinks.click(function(e) {
     e.preventDefault();
   });
-
+  
   function postMessage() {
-    var message = $('<div class="b-message b-message_you" style="opacity: 0.5"><div class="b-message__head"><div class="b-message__head__user">Leonya</div><div class="b-message__head__time">Aug 31, 2010 15:35</div></div><div class="b-message__body">' + messageArea.val() + '</div></div>');
+    var message = 'cmd=' + encodeURIComponent('msg chat/room_1 ' + messageArea.val());
+    
+    var client = new XMLHttpRequest();
+    client.onreadystatechange = function() {};
+    client.open("POST", "/xiva");
+    client.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');  
+    client.send(message);
+  }
+
+  function receiveMessage(messageText) {
+    if (messageText === "ping\n") return;
+    
+    var message = $('<div class="b-message b-message_you" style="opacity: 0.5"><div class="b-message__head"><div class="b-message__head__user">Leonya</div><div class="b-message__head__time">Aug 31, 2010 15:35</div></div><div class="b-message__body">' + messageText + '</div></div>');
     bMessages.append(message);
     message.animate({ opacity: 1 }, 1000);
     bMessages.scrollTop(10000);
