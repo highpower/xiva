f9c53fde793ee9b57608c931781636c7d4daa388
ponomarev <ponomarev@yandex-team.ru>
2011-01-14 16:56:03 +0300

    add ping formatter

---
+++ include/xiva/ping_formatter.hpp
@@ -0,0 +1,46 @@
+// xiva (acronym for HTTP Extended EVent Automata) is a simple HTTP server.
+// Copyright (C) 2009 Yandex <highpower@yandex.ru>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef XIVA_PING_FORMATTER_HPP_INCLUDED
+#define XIVA_PING_FORMATTER_HPP_INCLUDED
+
+#include <string>
+
+#include "xiva/config.hpp"
+#include "xiva/shared.hpp"
+
+namespace xiva {
+
+
+class XIVA_API ping_formatter : public shared {
+
+public:
+	ping_formatter();
+	virtual ~ping_formatter();
+
+	virtual std::string message() const = 0;
+	virtual std::string message_for_single() const = 0;
+
+private:
+	ping_formatter(ping_formatter const &);
+	ping_formatter& operator = (ping_formatter const &);
+};
+
+
+} // namespace
+
+#endif // XIVA_PING_FORMATTER_HPP_INCLUDED
---
+++ library/ping_formatter.cpp
@@ -0,0 +1,13 @@
+#include "acsetup.hpp"
+#include "xiva/ping_formatter.hpp"
+
+namespace xiva {
+
+ping_formatter::ping_formatter()
+{
+}
+
+ping_formatter::~ping_formatter() {
+}
+
+} // namespace
